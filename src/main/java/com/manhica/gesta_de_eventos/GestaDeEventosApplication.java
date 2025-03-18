7911ac9ab507e7cc7804d931a9d7b3c00972db1d
Ian Manhica <ianmanhica4@gmail.com>
2025-03-18 07:24:14 +0200

    Initial commit

---
+++ src/main/java/com/manhica/gesta_de_eventos/GestaDeEventosApplication.java
@@ -0,0 +1,13 @@
+package com.manhica.gesta_de_eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaDeEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaDeEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manhica/gesta_de_eventos/GestaDeEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manhica.gesta_de_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaDeEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
